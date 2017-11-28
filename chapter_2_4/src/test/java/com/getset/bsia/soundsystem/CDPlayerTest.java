b52fb833ccc7bd8eb983eb2ba6364c6e2adeaab6
get-set <535993116@qq.com>
2017-11-28 12:53:06 +0800

    Wiring beans with XML

--- chapter_2_4/src/test/java/com/getset/bsia/soundsystem/CDPlayerTest.java
+++ chapter_2_4/src/test/java/com/getset/bsia/soundsystem/CDPlayerTest.java
@@ -1,36 +1,37 @@
 package com.getset.bsia.soundsystem;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:soundsystem.xml")
 public class CDPlayerTest {
-
-    private ApplicationContext context;
 
     @Rule
     public StandardOutputStreamLog log = new StandardOutputStreamLog();
 
-    @Before
-    public void prepareContext() {
-        this.context = new ClassPathXmlApplicationContext("xmls/soundsystem.xml");
-    }
+
+    @Autowired
+    private CompactDisk sgtPeppers;
+
+    @Autowired
+    private CompactDisk blankDisk;
 
     @Test
     public void sgtPeppersTest() {
-        CompactDisk sgtPeppers = (SgtPeppers)context.getBean("sgtPeppers");
         sgtPeppers.play();
         assertEquals("Playing Sgt. Papper's Lonely hearts Club Band by The Beetles\n", log.getLog());
     }
 
     @Test
     public void blankDiskTest() {
-        CompactDisk blankDisk = (BlankDisk)context.getBean("blankDisk");
         blankDisk.play();
         assertEquals("Playing Sgt. Papper's Lonely hearts Club Band by The Beetles\nmusic 1  music 2  music 3  music 4  ", log.getLog());
     }
